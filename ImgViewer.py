593d833b5e7669f047e32a2fd9c96933db03b5a7
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2007-10-12 17:32:36 +0000

    800x800 default for standalone image viewer
    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/mxdc/trunk@52 5223631e-b783-4acc-b1a2-6de64d89f074

--- ImgViewer.py
+++ ImgViewer.py
@@ -708,7 +708,7 @@ def main():
     win.connect("destroy", lambda x: gtk.main_quit())
     win.set_border_width(6)
     win.set_title("Diffraction Image Viewer")
-    myview = ImgViewer(size=960)
+    myview = ImgViewer(size=800)
     hbox = gtk.HBox(False)
     hbox.pack_start(myview)
     win.add(hbox)
