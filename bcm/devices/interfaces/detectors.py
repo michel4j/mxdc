9de93e473c5f33314922caa761e39a26f7639363
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-03-14 23:24:36 +0000

    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@133 5223631e-b783-4acc-b1a2-6de64d89f074

---
+++ bcm/devices/interfaces/detectors.py
@@ -0,0 +1,104 @@
+from zope.interface import Interface
+
+class ICounter(Interface):
+    """This interface provides a handler for counter objects"""
+            
+    def count(time):
+        """
+        Integrate the counter
+        :Parameters:
+            - `time`: [float]
+                Duration to integrate object
+        :Return:
+            - the integrated counts [float]
+        
+        """
+
+    def getValue():
+        """
+        Get the instantaneous value of the object
+        :Return:
+            - the value [float]
+            
+        """
+
+    
+class IMultiChannelAnalyzer(ICounter):
+    """Interface for MultiChannelAnalyzer objects"""
+    
+    def setCooling(state):
+        """
+        Set the cooling  the device if cooling is available
+        :Parameters:
+            - `state` : [boolean]
+                State of cooling, True means cooling is on.
+        
+        """
+        
+    def channelToEnergy(channel):
+        """
+        Convert Channel to Energy
+        :Parameters:
+            - `channel` : [integer]
+                channel number to convert.
+        :Return:
+            - energy value corresponding to channel [float]
+        
+        """
+    
+    def energyToChannel(energy):
+        """
+        Convert Energy to channel number
+        :Parameters:
+            - `energy` : [float]
+                Energy to convert.
+        :Return:
+            - channel number [integer]
+        
+        """
+        
+    def setROI(roi=None):
+        """
+        Set the region of interest
+        :Parameters:
+            - `roi` : [(int,int)] default all 
+                A tuple of 2 integers for start and end region.
+        
+        """
+
+    def setEnergy(energy, width):
+        """
+        Set the region of interest by energy
+        :Parameters:
+            - `energy` : [float]
+                Median energy of desired region of interest.
+            - `width`: [float]
+                Width of region of interest in energy units          
+        
+        """
+        
+    def getSpectrum():
+        """
+        Get the full spectrum of the MultiChannelAnalyzer
+        :Return:
+            - spectrum [a tuple of 2 1-dim arrays of floats] in the order
+                (energies, channels)
+         
+        """
+
+    def acquire(time, wait=False):
+        """
+        Acquire Data from device
+        :Parameters:
+            - `time`: [float] > 0.0
+                Duration to acquire for
+            - `wait`: [boolean] default False
+                Whether to wait until acquire is done before returning
+        
+        """
+        
+    def wait():
+        """
+        Wait for MultiChannel Analyzer to complete task
+        
+        """  
---
+++ bcm/devices/interfaces/gobject.py
@@ -0,0 +1,44 @@
+from zope.interface import Interface
+
+class IGObject(Interface):
+    """Generic GObject interface"""
+    
+    def emit(detailed_signal, *args):
+        """
+        Emit a signal
+        :Parameters:
+            - `detailed_signal`: [string]
+                Signal name
+            - *args:
+                additional parameters, The additional parameters must 
+                match the number and type of the required signal handler 
+                parameters
+        :Returns:
+            - a PyObject*
+        
+        """
+        
+    def connect(detailed_signal, handler, *args):
+        """
+        Add a function or method to the list of signal handlers of the object.
+        :Parameters:
+            - `detailed_signal`: [string]
+                Signal name
+            - `handler`:
+                a Python function or method object
+            - *args:
+                An optional set of parameters to be passed to the signal
+                handler when invoked.            
+        :Returns:
+            - handler identifier [integer]
+        
+        """
+
+    def disconnect(handler_id):
+        """
+        Remove the signal handler from the list of signal handlers of the object.
+        :Parameters:  
+            - `handler_id`:  [integer]
+                an integer handler identifier
+            
+        """
