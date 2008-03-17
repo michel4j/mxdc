ca99bd09677f368cd559fc3ec1e468f39bb2f4a3
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-03-17 18:46:09 +0000

    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@136 5223631e-b783-4acc-b1a2-6de64d89f074

--- bcm/devices/normalizer.py
+++ bcm/devices/normalizer.py
@@ -9,20 +9,20 @@ class Normalizer(threading.Thread):
         self.start_counting = False
         self.stopped = False
         self.interval = 0.05
-        self.set_time(1.0)
+        self.setTime(1.0)
         self.device = dev
         self.first = 1.0
         self.factor = 1.0
 
-    def get_factor(self):
+    def getFactor(self):
         return self.factor
 
-    def set_time(self, t=1.0):
+    def setTime(self, t=1.0):
         self.duration = t
         self.accum = numpy.zeros( (self.duration / self.interval), numpy.float64)
     
     def initialize(self):
-        self.first = self.device.get_value()
+        self.first = self.device.getValue()
         
     def stop(self):
         self.stopped = True
@@ -35,7 +35,7 @@ class Normalizer(threading.Thread):
         self.initialize()
         self.count = 0
         while not self.stopped:
-            self.accum[ self.count ] = self.device.get_value()
+            self.accum[ self.count ] = self.device.getValue()
             self.count = (self.count + 1) % len(self.accum)
             self.factor = self.first/numpy.mean(self.accum)
             time.sleep(self.interval)
