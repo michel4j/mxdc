3eb879428180cd8cca8a02902999ade2acce0d4a
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-03-17 22:00:13 +0000

    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@142 5223631e-b783-4acc-b1a2-6de64d89f074

---
+++ bcm/devices/shutters.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys, time
+import gtk, gobject
+from EPICS import PV
+from LogServer import LogServer
+
+class AbstractShutter(gobject.GObject):
+    __gsignals__ =  { 
+                    "changed": ( gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_BOOLEAN,)  ),
+                    }  
+    def __init__(self, name=None):
+        gobject.GObject.__init__(self)
+        self.state = True
+        self.name = name
+
+    def open(self):
+        self.state = True
+        LogServer.log( "%s opened" % self.name)        
+            
+    def close(self):
+        self.state = False
+        LogServer.log( "%s closed" % self.name)        
+
+    def is_open(self):
+        return self.state
+
+    def get_name(self):
+        return self.name
+
+class EpicsShutter(AbstractShutter):
+    def __init__(self, name=None):
+        AbstractShutter.__init__(self, name)
+        self.open_cmd = PV("%s:opr:open" % name)
+        self.close_cmd = PV("%s:opr:close" % name)
+        self.state = PV("%s:state" % name)
+        self.last_state = self.state.get()
+        gobject.timeout_add(250, self._queue_check)
+    
+    def _check_change(self):
+        state = self.is_open()
+        if state != self.last_state:
+            gobject.idle_add(self.emit,'changed', state)
+        self.last_state = state
+        return False
+
+    def _queue_check(self):
+        gobject.idle_add(self._check_change)
+        return True
+            
+    def is_open(self):
+        return self.state.get() == 1
+    
+    def open(self):
+        self.open_cmd.put(1)
+        LogServer.log( "%s opened" % self.name)
+    
+    def close(self):
+        self.close_cmd.put(1)
+        LogServer.log( "%s closed" % self.name)        
+               
+FakeShutter = AbstractShutter
+        
+# Register objects with signals
+gobject.type_register(AbstractShutter)
+        
+    
