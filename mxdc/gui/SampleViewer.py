499e7699fb2427ae133a853986dcd28e56704064
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-04-16 17:02:28 +0000

    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@279 5223631e-b783-4acc-b1a2-6de64d89f074

--- mxdc/gui/SampleViewer.py
+++ mxdc/gui/SampleViewer.py
@@ -488,27 +488,3 @@ class SampleViewer(gtk.HBox):
         self.light.move_to( self.lighting )
         return True
                     
-
-def main():
-    import bcm.beamline
-    
-    win = gtk.Window()
-    win.connect("destroy", lambda x: gtk.main_quit())
-    win.set_border_width(0)
-    win.set_title("SampleViewer")
-    book = gtk.Notebook()
-    win.add(book)
-    bl = bcm.beamline.PX('08id1-sample.conf')
-    bl.setup()
-    myviewer = SampleViewer(bl)
-    book.append_page(myviewer, tab_label=gtk.Label('Sample Viewer') )
-    win.show_all()
-
-    try:
-        gtk.main()
-    finally:
-        myviewer.video.stop()
-
-
-if __name__ == '__main__':
-    main()
