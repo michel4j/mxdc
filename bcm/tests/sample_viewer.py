7487df7b06c23dced1dbbeb116ab1320ff9f4876
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-04-16 17:02:28 +0000

    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@279 5223631e-b783-4acc-b1a2-6de64d89f074

---
+++ bcm/tests/sample_viewer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os, sys
+sys.path.append(os.environ['BCM_PATH'])
+
+from bcm.beamline import PX
+from mxdc.gui import SampleViewer
+
+def main():
+
+    win = gtk.Window()
+    win.connect("destroy", lambda x: gtk.main_quit())
+    win.set_border_width(0)
+    win.set_title("SampleViewer")
+    book = gtk.Notebook()
+    win.add(book)
+    bl = PX('08id1-sample.conf')
+    bl.setup()
+    myviewer = SampleViewer(bl)
+    book.append_page(myviewer, tab_label=gtk.Label('Sample Viewer') )
+    win.show_all()
+
+    try:
+        gtk.main()
+    finally:
+        myviewer.video.stop()
+
+
+if __name__ == '__main__':
+    main()
