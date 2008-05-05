791533af63b61096a7d80100fd414eef43b5a561
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-05-05 15:00:46 +0000

    New Splash Screen
    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@355 5223631e-b783-4acc-b1a2-6de64d89f074

--- mxdc/gui/Splash.py
+++ mxdc/gui/Splash.py
@@ -1,22 +1,44 @@
 import gtk, gobject
 import sys, os
+from ActiveWidgets import LinearProgress
 
 class Splash(object):
-    def __init__(self, image, startup_obj):
+    def __init__(self, image, startup_obj, icon=None, logo=None):
         self.win = gtk.Window()
         self.win.set_size_request(480,290)
         self.win.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_SPLASHSCREEN)
-        self.win.set_gravity(gtk.gdk.GRAVITY_CENTER)
+        #self.win.set_gravity(gtk.gdk.GRAVITY_CENTER)
 
-        self.img = gtk.Image()
-        self.img.set_from_file(image)
+        pixbuf = gtk.gdk.pixbuf_new_from_file(image)
+        pixmap, mask = pixbuf.render_pixmap_and_mask()
+        self.win.set_app_paintable(True)
+        self.win.resize(width, height)
+        self.win.realize()
+        self.win.set_back_pixmap(pixmap, False)
+
         vbox = gtk.VBox(False,0)
-        vbox.pack_start(self.img, expand=False, fill=False)
+        hbox = gtk.HBox(False, 0)
 
-        self.pbar = gtk.ProgressBar()
-        vbox.pack_end(self.pbar)
+        self.pbar = LinearProgress()
+        self.pbar.set_size_request(0,8)
+        self.log = gtk.Label()
+        self.log.set_alignment(0,0.5)
+        self.icon = gtk.Image()
+        self.logo = gtk.Image()
+        if icon:
+            self.icon.set_from_file(icon)
+        if logo:
+            self.logo.set_from_file(title)
+        hbox.pack_start(self.icon, expand=False, fill=False)
+        hbox.pack_start(self.logo)
+        vbox.pack_start(hbox)
+        vbox.pack_end(self.pbar, expand=False, fill=False)
+        vbox.pack_end(self.log, expand=False, fill=False)
+        vbox.set_spacing(4)
+        vbox.set_border_width(16)
+        
         self.win.add(vbox)
-        self.win.set_position(gtk.WIN_POS_CENTER)                
+        #self.win.set_position(gtk.WIN_POS_CENTER)                
         self.win.show_all()
         self.win.realize()
 
@@ -31,5 +53,5 @@ class Splash(object):
         self.pbar.set_fraction(frac)
     
     def on_log(self, obj, text):
-        self.pbar.set_text(text)
+        self.log.set_text(text)
     
