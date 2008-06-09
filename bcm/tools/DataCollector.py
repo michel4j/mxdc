acc0d1caa68b0c1a720f2d42472a19ce1299682f
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-06-09 17:19:36 +0000

    test moving fix
    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@412 5223631e-b783-4acc-b1a2-6de64d89f074

--- bcm/tools/DataCollector.py
+++ bcm/tools/DataCollector.py
@@ -99,7 +99,7 @@ class DataCollector(gobject.GObject):
                 self.distance.move_to(frame['distance'])
             if abs(self.energy.get_position() - frame['energy']) > 0.0005:
                 self.energy.move_to(frame['energy'])
-            if abs(self.two_theta.get_position() - frame['two_theta']) > 0.05
+            if abs(self.two_theta.get_position() - frame['two_theta']) > 0.05:
                 self.two_theta.move_to(frame['two_theta'])
             self.two_theta.wait()
             self.distance.wait()
