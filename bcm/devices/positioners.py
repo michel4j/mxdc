f9e90ab86448f4eed79394af48d42b4b2bb78801
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-07-11 16:42:54 +0000

    bug fixing
    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@425 5223631e-b783-4acc-b1a2-6de64d89f074

--- bcm/devices/positioners.py
+++ bcm/devices/positioners.py
@@ -80,6 +80,7 @@ class MotorBase(gobject.GObject):
 
     def _signal_request(self, obj, value):
         #self._log( "move to %f %s requested" % (value, self.units) )
+        pass
 
            
     def _signal_health(self, obj, state):
