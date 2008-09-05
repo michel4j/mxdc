d0aee57480f23fdb81f1cec531c0b17eee1a735f
michel <michel@5223631e-b783-4acc-b1a2-6de64d89f074>
2008-09-05 17:13:03 +0000

    git-svn-id: svn+ssh://opi1608b-002/home/michel/svn/mxdc/branches/bcm-branch@487 5223631e-b783-4acc-b1a2-6de64d89f074

--- mxdc/gui/StatusPanel.py
+++ mxdc/gui/StatusPanel.py
@@ -11,7 +11,7 @@ class StatusPanel(gtk.VBox):
         self.layout_table.set_border_width(1)
 
         self.clock = gtk.Label()                        
-        self.layout_table.attach(self.__frame_control('',self.clock, gtk.SHADOW_ETCHED_IN), 10, 11 , 0, 1)
+        self.layout_table.attach(self.__frame_control('08ID-1',self.clock, gtk.SHADOW_ETCHED_IN), 10, 11 , 0, 1)
 
         self.intensity = VariableLabel(beamline.ring_current, format="%10.1f")
         self.layout_table.attach(self.__frame_control('Cur<sub>mA</sub>', self.intensity, gtk.SHADOW_IN), 9, 10 , 0, 1)
@@ -22,7 +22,7 @@ class StatusPanel(gtk.VBox):
         self.intensity = VariableLabel(beamline.i0, format="%10.2g")
         self.layout_table.attach(self.__frame_control('I0<sub>A</sub>', self.intensity, gtk.SHADOW_IN), 7, 8 , 0, 1)
         
-        gobject.timeout_add(500,self.update_clock)
+        #gobject.timeout_add(500,self.update_clock)
         hseparator = gtk.HSeparator()
         hseparator.set_size_request(-1,3)
         self.pack_start(hseparator, expand= False, fill=False, padding=0)
