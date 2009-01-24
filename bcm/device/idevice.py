2d26f4a9327be09d0a8c38546ab43cf704a2abbc
Michel Fodje <michel.fodje@lightsource.ca>
2009-01-24 00:31:28 -0600

    Reorganizing interface classes

---
+++ bcm/device/idevice.py
@@ -0,0 +1,335 @@
+"""This module defines interfaces for Beamline Control Module."""
+
+from zope.interface import Interface, Attribute
+
+
+class IAutomounter(Interface):
+    
+    """A sample automounter device object."""
+     
+    def mount(address, wash=False):
+        """Pick up a sample and mount in on the goniometer.
+        
+        Arguments:
+        address -- location of sample in automounter
+        
+        Keyword arguments:
+        wash    -- whether to wash the sample or not (default False)       
+        """
+    
+    def dismount(address=None):
+        """Dismount a sample from the goiniometer.
+        
+        Keyword arguments:
+        address -- location to store sample (default None)
+        
+        The sample is stored at the given address if specified, 
+        otherwise it is replaced at the location from which it
+        was mounted.        
+        """
+    
+    def mount_next():
+        """Mount next sample in sequence."""
+    
+    def probe(mask):
+        """Check automounter locations for sample presence and accessibility.
+        
+        Arguments:
+        mask -- a string defining locations to probe        
+        """
+
+    def get_state():
+        """Return the current state of the device."""
+    
+        
+    def wait():
+        """Wait for automounter to become idle."""
+        
+    
+    def stop():
+        """Terminate all automounter operations and go to home position."""
+  
+
+
+class ICollimator(Interface):
+    
+    """An X-ray beam collimator device object."""
+    
+    width = Attribute("A motor controlling the horizontal gap.")
+    height = Attribute("A motor controlling the vertical gap.")
+    
+    def set_width(width):
+        """Set the horizontal gap of the collimator.
+        
+        Arguments:
+        width -- horizontal gap of collimator
+        """
+   
+    def set_height(height):
+        """Set the vertical gap of the collimator.
+        
+        Arguments:
+        height -- vertical gap of collimator
+        """
+
+    def wait():
+        """Wait for collimator to become idle."""
+
+    def stop():
+        """Terminate all collimator operations."""
+
+    def get_state():
+        """Return the current state of the device."""
+
+
+class ICounter(Interface):
+
+    """An integrating counter object."""
+            
+    def count(time):
+        """
+        Integrate the counter for a specified duration and returns total count.
+        
+        Arguments:
+        time --   Duration to integrate object in seconds 
+        
+        Returns the total number of counts.       
+        """
+
+class IMonochromator(Interface):
+
+    """A monochromator device object."""
+    
+    energy = Attribute("Full monochromator energy motor")
+    bragg_energy = Attribute("Simple monochromator energy motor.")
+    
+    def optimize():
+        """Start an optimization sequence."""
+    
+    def get_state():
+        """Return the current state of the device."""
+        
+    def wait():
+        """Wait for monochromator to become idle."""
+        
+    def stop():
+        """Terminate all monochromator operations."""
+
+
+class IGoniometer(Interface):
+
+    """A goniometer device object."""
+    
+    phi = Attribute("Goniometer phi motor.")
+    kappa = Attribute("Goniometer kappa motor.")
+    omega = Attribute("Goniometer omega motor.")
+    
+    def configure(time=1.0, delta=1.0, angle=0.0):
+        """Configure the goniometer scan parameters.
+        
+        Keyword arguments:
+        time    -- scan exposure time in seconds (default 1.0)
+        delta   -- scan range in degrees (default 1.0) 
+        angle   -- starting angle position for scan in degrees (default 0.0)
+        """
+    
+    def scan():
+        """Start the scan operation."""
+    
+    def get_state():
+        """Return the current state of the device."""
+        
+    def wait():
+        """Wait for goniometer to become idle."""
+        
+    def stop():
+        """Terminate all goniometer operations."""
+    
+  
+class IShutter(Interface):
+
+    """A shutter device object."""
+    
+    def open():
+        """Open the shutter."""
+            
+    def close():
+        """Close the shutter."""
+
+    def get_state():
+        """Return the current state of the device."""
+
+
+class IImagingDetector(Interface):
+
+    """An imaging detector device for aquiring image frames."""
+       
+    def initialize():
+        """Reset and initialize the detector."""
+        
+    def start():
+        """Start acquiring."""
+                
+    def save(props):
+        """Stop acquiring and save the image.
+        
+        Arguments:
+        props    -- a dictionary of property name, value pairs to set
+        valid keys for props are:
+            delta       -- rotation range in degrees for this frame
+            distance    -- diffractometer distance for this frame
+            time        -- exposure time for this frame
+            angle       -- starting angle position for this frame
+            index       -- frame number
+            energy      -- beam energy for this frame
+            prefix      -- file name prefix            
+            filename    -- name of image file to save
+            directory   -- directory to save image           
+        
+        """
+    
+    def wait():
+        """Wait for detector to become idle."""
+        
+    def stop():
+        """Terminate all detector operations."""
+
+    def get_state():
+        """Return the current state of the device."""
+
+
+
+class IPositioner(Interface):
+
+    """A positioning device object.""" 
+
+    def set(pos):
+        """Set the position of the device.
+        
+        Arguments:
+        pos -- the target position to set to
+        
+        """
+        
+    def get():
+        """Return the current position of the device."""
+
+
+
+class IDiffractometer(Interface):
+
+    """A diffractometer device object."""
+    
+    distance = Attribute("Detector distance motor.")
+    two_theta = Attribute("Detector swing-out angle motor.")
+
+    def wait():
+        """Wait for diffractometer to become idle."""
+        
+    def stop():
+        """Terminate all diffractometer operations."""
+
+    def get_state():
+        """Return the current state of the device."""
+
+
+class IMultiChannelAnalyzer(ICounter):
+
+    """A Multi Channel Analyzer device object"""
+    
+    def configure(props):
+        """Configure the properties of the device.
+        
+        Arguments: properties        
+        props    -- a dictionary of property name, value pairs to set
+        valid keys for props are:
+            cooling     -- boolean value, whether to turn cooling on or off
+            energy      -- set the position of the regioin of interest. None 
+                           resets to the full range.        
+
+        """
+        
+    def acquire(time):
+        """Acquire a full spectrum of data and return an array of the spectrum.
+        
+        Arguments:
+        time    -- scan duration in seconds.
+
+        Returns a 2-dimentional array with the first column being the energy
+        values, and the second column being the corresponding counts.
+           
+        """
+        
+    def wait():
+        """Wait for Multi Channel Analyzer to complete all tasks."""
+
+    def stop():
+        """Terminate all operations."""
+
+    def get_state():
+        """Return the current state of the device."""
+
+
+
+class IMotor(Interface):
+
+    """A Motor device object."""
+
+    def configure(props):
+        """Configure the properties of the device.
+        
+        Arguments: properties        
+        props    -- a dictionary of property name, value pairs to set
+        valid keys for props are:
+            calib   -- boolean value, whether to motor is calibrated or not
+            reset   -- reset the position to the given value        
+
+        """
+
+    def move_to(pos):
+        """Move the motor to the specified position.
+        
+        Arguments:
+        pos -- the target position to move to.
+        
+        """
+        
+    def move_by(incr):
+        """Move the motor to the relative to the current position.
+        
+        Arguments:
+        incr -- the relative increment to move by.
+        
+        """
+        
+    def get_position():
+        """Return the current position of the motor."""
+
+    def wait():
+        """Wait for motor to stop moving."""
+               
+    def stop():  
+        """Stop Motor movement."""
+    
+    def get_state():
+        """Return the current state of the device."""
+
+
+class ICamera(Interface):
+
+    """A camera device object providing a video source."""
+            
+    def get_frame():
+        """Get current frame of video.
+        
+        Returns an image of a frame of video data.
+        
+        """
+                  
+    def save(filename):
+        """Save current frame to file.
+        
+        Arguments:
+        filename    -- name of file to save image to.
+
+        """
