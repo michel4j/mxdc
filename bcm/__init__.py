524d81d661ecc0ed952948b5925e6fc83ccfd029
michel <michel@localhost.localdomain>
2009-02-04 16:21:08 -0600

    Bug - adapter_hooks screws script plugins

--- bcm/__init__.py
+++ bcm/__init__.py
@@ -5,7 +5,6 @@ from zope.interface.interface import adapter_hooks
 registry = AdapterRegistry()
 
 def _hook(provided, object):
-    print provided, object
     adapter = registry.lookup1(providedBy(object),
                                provided, '')
     return adapter(object)
