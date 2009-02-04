d88ccc345148dff0afac9ce76c53eee7ccf55915
michel <michel@localhost.localdomain>
2009-02-04 15:55:04 -0600

    Started cleanup of MXDC

--- bcm/__init__.py
+++ bcm/__init__.py
@@ -5,6 +5,7 @@ from zope.interface.interface import adapter_hooks
 registry = AdapterRegistry()
 
 def _hook(provided, object):
+    print provided, object
     adapter = registry.lookup1(providedBy(object),
                                provided, '')
     return adapter(object)
