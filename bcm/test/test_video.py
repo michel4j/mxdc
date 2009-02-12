509c44e64638a8fd4ddab7beafc6ef0ae041b274
michel <michel@localhost.localdomain>
2009-02-12 17:39:26 -0600

    Removed threading from video src. Replaced with producer-consumer pattern

--- bcm/test/test_video.py
+++ bcm/test/test_video.py
@@ -1,7 +1,8 @@
 import sys, os
 import gtk
-sys.path.append('/media/seagate/beamline-control-module')
-from bcm.device.video import SimCamera
+sys.path.append('/home/michel/Code/eclipse-ws/beamline-control-module')
+from bcm.device.video import AxisCamera, CACamera
+from bcm.device.motor import CLSMotor
 from mxdc.widgets.video import VideoWidget
 from bcm.utils.log import log_to_console
 
@@ -9,14 +10,20 @@ def main():
     win = gtk.Window()
     win.connect("destroy", lambda x: gtk.main_quit())
     win.set_border_width(0)
+    #win.set_size_request(320,240)
     win.set_title("Video Test")
     
-    cam = SimCamera()
+    #cam = AxisCamera('10.52.4.102')
+    cam = CACamera('CAM1608-001:data', CLSMotor('SMTR16083I1021:mm'))
     vid = VideoWidget(cam)
     win.add(vid)
     win.show_all()
     
-    gtk.main()
+    try:
+
+        gtk.main()
+    finally:
+        cam.stop()
 
 
 if __name__ == '__main__':
