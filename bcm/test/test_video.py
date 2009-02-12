794d3b47586e6867287770b34810e6195ecced9e
Michel Fodje <michel.fodje@lightsource.ca>
2009-02-12 09:07:43 -0600

    Testing video widgets

---
+++ bcm/test/test_video.py
@@ -0,0 +1,26 @@
+import sys, os
+import gtk
+sys.path.append('/media/seagate/beamline-control-module')
+from bcm.device.video import SimCamera
+from mxdc.widgets.video import VideoWidget
+from bcm.utils.log import log_to_console
+
+def main():
+    win = gtk.Window()
+    win.connect("destroy", lambda x: gtk.main_quit())
+    win.set_border_width(0)
+    win.set_title("Video Test")
+    
+    cam = SimCamera()
+    vid = VideoWidget(cam)
+    win.add(vid)
+    win.show_all()
+    
+    try:
+        gtk.main()
+    finally:
+        vid.stop()
+
+
+if __name__ == '__main__':
+    main()  
