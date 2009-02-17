fa4ba133c0d774f80bb16ad631295bef2ba0d291
Michel Fodje <michel.fodje@lightsource.ca>
2009-02-17 12:59:43 -0600

    Implementing Scan manager changes

--- bcm/test/detector_test.py
+++ bcm/test/detector_test.py
@@ -1,8 +1,5 @@
 import time
 from bcm.device.detector import MXCCDImager
-from bcm.utils.console import event_loop
-
-event_loop.start()
 
 det = MXCCDImager('BL08ID1:CCD', 3072, 0.007234)
 det.wait()
@@ -12,5 +9,3 @@ time.sleep(1.0)
 det.save({})
 det.wait()
 
-
-event_loop.stop()
