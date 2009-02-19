154cff97207b1d6d0f2dbb2eaed41dfe50da7bb1
michel4j@gmail.com <michel@localhost.localdomain>
2009-02-18 22:01:28 -0600

    Changes in scanmanager

--- bcm/engine/spectroscopy.py
+++ bcm/engine/spectroscopy.py
@@ -11,88 +11,94 @@ from bcm.utils.log import get_module_logger
 _logger = get_module_logger(__name__)
 
 class XRFScan(BasicScan):    
-    def __init__(self, t=0.5, energy=None):
+    def __init__(self, t=0.5, energy=None, attenuation=0.0):
         BasicScan.__init__(self)
-        self.configure(t, energy)
+        self.configure(t, energy, attenuation)
         
-    def configure(self, t, energy):
+    def configure(self, t, energy, attenuation):
         try:
             self.beamline = gsm.getUtility(IBeamline, 'bcm.beamline')
         except:
+            self.beamline = None
             _logger.error('Beamline unavailable')
         self._energy = energy
         self._duration = t
+        self._attenuation = attenuation
         self.data = []
                             
     def run(self):
         _logger.debug('Exitation Scan waiting for beamline to become available.')
+        if self.beamline is None:
+            _logger.error('Beamline unavailable')
+            return
         self.beamline.lock.acquire()
         try:
             _logger.debug('Exitation Scan started')
             gobject.idle_add(self.emit, 'started')     
             self.beamline.mca.configure(cooling=True, roi=None)
+            self.beamline.attenuator.set(self._attenuation)
             if self._energy is not None:
-                self.beamline.energy.move_to(self._energy)
-                self.beamline.energy.wait()
+                self.beamline.monochromator.energy.move_to(self._energy)
+                self.beamline.monochromator.energy.wait()
             self.beamline.exposure_shutter.open()
             self.data = self.beamline.mca.acquire(t=self._duration)
             gobject.idle_add(self.emit, "done")
             gobject.idle_add(self.emit, "progress", 1.0 )
         finally:
             self.beamline.exposure_shutter.close()
+            self.beamline.attenuator.set(0.0)
             self.beamline.lock.release()
             
 
 class XANESScan(BasicScan):
-    def __init__(self, edge='Se-K', t=0.5):
+    def __init__(self, edge='Se-K', t=0.5, attenuation=0.0):
         BasicScan.__init__(self)
-        self.configure(edge, t)
+        self.configure(edge, t, attenuation)
         
-    def configure(self, edge, t):
+    def configure(self, edge, t, attenuation):
         try:
             self.beamline = gsm.getUtility(IBeamline, 'bcm.beamline')
         except:
+            self.beamline = None
             _logger.error('Beamline unavailable')
         self._duration = t
         self._energy_db = get_energy_database()
         self._edge, self._energy = self._energy_db[edge]
         self._targets = xanes_targets(self._energy)
+        self._attenuation = attenuation
+        self.data = []
               
-    def run(self):       
+    def run(self):
+        if self.beamline is None:
+            _logger.error('Beamline unavailable')
+            return
         _logger.info('Edge Scan waiting for beamline to become available.')
         self.beamline.lock.acquire()
         try:
             gobject.idle_add(self.emit, 'started')
             _logger.info('Edge Scan started.')
+            self.beamline.attenuator.set(self._attenuation)
             self.beamline.mca.configure(cooling=True, roi=self._energy)
-            self.beamline.energy.move_to(self._energy)
-            self.beamline.energy.wait()   
+            self.beamline.monochromator.energy.move_to(self._energy)
+            self.beamline.monochromator.energy.wait()   
                    
             self.count = 0
             self.beamline.exposure_shutter.open()
-            self.beamline.mca.erase()
             _logger.info("%4s %15s %15s %15s" % ('#', 'Energy', 'Counts', 'Scale Factor'))
             for x in self.energy_targets:
-                if self.stopped:
-                     break
+                if self._stopped:
+                    _logger.info( "Scan stopped!" )
+                    break
                     
                 self.count += 1
-                prev = self.beamline.bragg_energy.get_position()                
-                self.beamline.bragg_energy.move_to(x, wait=True)
-                if self.count == 1:
-                    self.first_intensity = (self.beamline.i0.count(0.5) * 1e9)
-                    self.factor = 1.0
-                else:
-                    self.factor = self.first_intensity/(self.beamline.i0.count(0.5)*1e9)
-                y = self.beamline.mca.count(self.time)
+                self.beamline.monochromator.simple_energy.move_to(x, wait=True)
+                y = self.beamline.mca.count(self._duration)
+                i0 = self.beamline.i0.count(self._duration)
+                self.data.append( [self.count, x, y, i0, y / i0] )
                     
-                y = y * self.factor
-                self.x_data_points.append( x )
-                self.y_data_points.append( y )
-                
                 fraction = float(self.count) / len(self.energy_targets)
-                _logger.info("%4d %15g %15g %15g" % (self.count, x, y, self.factor))
-                gobject.idle_add(self.emit, "new-point", x, y )
+                _logger.info("%4d %15g %15g %15g %15g" % (count, x, y, i0, y / i0))
+                gobject.idle_add(self.emit, "new-point", x, (y/i0) )
                 gobject.idle_add(self.emit, "progress", fraction )
                              
             if self.stopped:
@@ -103,7 +109,8 @@ class XANESScan(BasicScan):
                 gobject.idle_add(self.emit, "done")
                 gobject.idle_add(self.emit, "progress", 1.0 )
         finally:
-            self.beamline.shutter.close()
+            self.beamline.exposure_shutter.close()
+            self.beamline.attenuator.set(0.0)
             self.beamline.lock.release()
               
 
