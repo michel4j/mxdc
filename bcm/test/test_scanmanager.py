9f2005151f793ce2042906eaae525c27ee1bf475
Michel Fodje <michel.fodje@lightsource.ca>
2009-02-18 22:01:28 -0600

    Changes in scanmanager

--- bcm/test/test_scanmanager.py
+++ bcm/test/test_scanmanager.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
+from twisted.internet import glib2reactor
+glib2reactor.install()
+from twisted.internet import reactor
 import gtk
 from bcm.utils import converter
-from mxdc.ScanManager import ScanManager
+from mxdc.widgets.scanmanager import ScanManager
 
 def main():
     win = gtk.Window()
-    win.connect("destroy", lambda x: gtk.main_quit())
+    win.connect("destroy", lambda x: reactor.stop())
     win.set_border_width(2)
     win.set_title("Periodic Table Demo")
     sm = ScanManager()
@@ -17,6 +20,6 @@ def main():
 if __name__ == '__main__':
     try:
         main()
-        gtk.main()
+        reactor.run()
     finally:
         print "Quiting..."
