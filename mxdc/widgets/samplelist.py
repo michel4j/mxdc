193bcde3878bfa4f766fe9bb2190fed9f72d0a51
Michel Fodje <michel.fodje@lightsource.ca>
2009-03-02 09:00:02 -0600

    Implementing screening manager and new splash

--- mxdc/widgets/samplelist.py
+++ mxdc/widgets/samplelist.py
@@ -5,15 +5,18 @@ from bcm.utils.configobj import ConfigObj
 from mxdc.widgets.dialogs import *
 
 (
+    SAMPLE_COLUMN_CONTAINER,
     SAMPLE_COLUMN_STATE,
     SAMPLE_COLUMN_SELECTED,
     SAMPLE_COLUMN_PORT,
     SAMPLE_COLUMN_CODE,
     SAMPLE_COLUMN_NAME,
     SAMPLE_COLUMN_COMMENTS,
-) = range(6)
+    SAMPLE_COLUMN_EDITABLE,
+) = range(8)
 
 COLUMN_DICT = {
+    SAMPLE_COLUMN_CONTAINER: 'container',
     SAMPLE_COLUMN_STATE: 'State', 
     SAMPLE_COLUMN_SELECTED: 'Selected',
     SAMPLE_COLUMN_PORT: 'Port',
@@ -39,32 +42,34 @@ STATE_DICT = {
 }
 
 TEST_DATA = \
-[(0, False, 'LA1',  '60482','Normal', 'scrollable notebooks and hidden tabs'),
- (0, False, 'LA2', '60620', 'Critical','gdk_window_clear_area(gdkwindow-win32.c)'),
- (0, False, 'LA3', '50214', 'Major', 'Xft support does not clean up correctly'),
- (0, True, 'LA4',  '52877', 'Major', 'GtkFileSelection needs a refresh method. '),
- (0, False, 'LA5', '56070', 'Normal', "Can't click button after setting in sensitive"),
- (0, True, 'LA6',  '56355', 'Normal', 'GtkLabel - Not all changes propagate correctly'),
- (0, False, 'LA7', '50055', 'Normal', 'Rework width/height computations for TreeView'),
- (0, False, 'LA8', '58278', 'Normal', "gtk_dialog_set_response_sensitive() doesn't work"),
- (0, False, 'LA9', '55767', 'Normal', 'Getters for all setters'),
- (0, False, 'LA10', '56925', 'Normal', 'Gtkcalender size'),
- (0, False, 'LA11', '56221', 'Normal', 'Selectable label needs right-click copy menu'),
- (0, True, 'LA12',  '50939', 'Normal', 'Add shift clicking to GtkTextView'),
- (0, False, 'LA13', '6112',  'Enhancement', 'netscape-like collapsable toolbars'),
- (0, False, 'LA14', '00001',    'Normal', 'First bug :=)')]
+[('unknown', 0, False, 'LA1',  '60482','Normal', 'scrollable notebooks and hidden tabs'),
+ ('unknown', 0, False, 'LA2', '60620', 'Critical','gdk_window_clear_area(gdkwindow-win32.c)'),
+ ('unknown', 0, False, 'LA3', '50214', 'Major', 'Xft support does not clean up correctly'),
+ ('unknown', 0, True, 'LA4',  '52877', 'Major', 'GtkFileSelection needs a refresh method. '),
+ ('unknown', 0, False, 'LA5', '56070', 'Normal', "Can't click button after setting in sensitive"),
+ ('unknown', 0, True, 'LA6',  '56355', 'Normal', 'GtkLabel - Not all changes propagate correctly'),
+ ('unknown', 0, False, 'LA7', '50055', 'Normal', 'Rework width/height computations for TreeView'),
+ ('unknown', 0, False, 'LA8', '58278', 'Normal', "gtk_dialog_set_response_sensitive() doesn't work"),
+ ('unknown', 0, False, 'LA9', '55767', 'Normal', 'Getters for all setters'),
+ ('unknown', 0, False, 'LA10', '56925', 'Normal', 'Gtkcalender size'),
+ ('unknown', 0, False, 'LA11', '56221', 'Normal', 'Selectable label needs right-click copy menu'),
+ ('unknown', 0, True, 'LA12',  '50939', 'Normal', 'Add shift clicking to GtkTextView'),
+ ('unknown', 0, False, 'LA13', '6112',  'Enhancement', 'netscape-like collapsable toolbars'),
+ ('unknown', 0, False, 'LA14', '00001',    'Normal', 'First bug :=)')]
 
 
 class SampleList(gtk.ScrolledWindow):
     def __init__(self):
         gtk.ScrolledWindow.__init__(self)
         self.listmodel = gtk.ListStore(
+            gobject.TYPE_STRING,
             gobject.TYPE_INT,
             gobject.TYPE_BOOLEAN,
             gobject.TYPE_STRING,
             gobject.TYPE_STRING,
             gobject.TYPE_STRING,
-            gobject.TYPE_STRING,           
+            gobject.TYPE_STRING,
+            gobject.TYPE_BOOLEAN,         
         )
                         
         self.listview = gtk.TreeView(self.listmodel)
@@ -78,27 +83,32 @@ class SampleList(gtk.ScrolledWindow):
         self.listview.connect('row-activated',self.on_row_activated)
 
     def load_data(self, data):
+        self.clear()
         for item in data:
             iter = self.listmodel.append()        
-            self.listmodel.set(iter, 
+            self.listmodel.set(iter,
+                SAMPLE_COLUMN_CONTAINER, item[SAMPLE_COLUMN_CONTAINER],
                 SAMPLE_COLUMN_STATE, item[SAMPLE_COLUMN_STATE], 
                 SAMPLE_COLUMN_SELECTED, item[SAMPLE_COLUMN_SELECTED],
                 SAMPLE_COLUMN_PORT, item[SAMPLE_COLUMN_PORT],
                 SAMPLE_COLUMN_CODE, item[SAMPLE_COLUMN_CODE],
                 SAMPLE_COLUMN_NAME, item[SAMPLE_COLUMN_NAME],
                 SAMPLE_COLUMN_COMMENTS, item[SAMPLE_COLUMN_COMMENTS],
+                SAMPLE_COLUMN_EDITABLE, False
             )
             
 
     def __add_item(self, item):
         iter = self.listmodel.append()        
-        self.listmodel.set(iter, 
+        self.listmodel.set(iter,
+            SAMPLE_COLUMN_CONTAINER, item['container'],
             SAMPLE_COLUMN_STATE, item['state'], 
             SAMPLE_COLUMN_SELECTED, item['selected'],
             SAMPLE_COLUMN_PORT, item['port'],
             SAMPLE_COLUMN_CODE, item['code'],
             SAMPLE_COLUMN_NAME, item['name'],
             SAMPLE_COLUMN_COMMENTS, item['comments'],
+            SAMPLE_COLUMN_EDITABLE, False,
         )
     
     def __set_color(self,column, renderer, model, iter):
@@ -120,11 +130,16 @@ class SampleList(gtk.ScrolledWindow):
         
         for key in [SAMPLE_COLUMN_PORT, SAMPLE_COLUMN_CODE, SAMPLE_COLUMN_NAME,SAMPLE_COLUMN_COMMENTS]:
             renderer = gtk.CellRendererText()
-            column = gtk.TreeViewColumn(COLUMN_DICT[key], renderer, text=key)
+            renderer.connect("edited", self._on_cell_edited, model, key)
+            column = gtk.TreeViewColumn(COLUMN_DICT[key], renderer, text=key, editable=SAMPLE_COLUMN_EDITABLE)
             column.set_cell_data_func(renderer, self.__set_color)
             #column.set_sort_column_id(key)        
             self.listview.append_column(column)
         self.listview.set_search_column(SAMPLE_COLUMN_NAME)
+
+    def _on_cell_edited(self, cell, path_string, new_text, model, column):
+        iter = model.get_iter_from_string(path_string)
+        model.set(iter, column, new_text)
         
     def set_row_selected(self, pos, selected=True):
         path = (pos,)
@@ -151,8 +166,100 @@ class SampleList(gtk.ScrolledWindow):
         model.set(iter, SAMPLE_COLUMN_SELECTED, (not value) )            
         return True
     
+    def on_edit_toggled(self, obj):
+        state = obj.get_active()
+        model = self.listview.get_model()
+        iter = model.get_iter_first()
+        while iter:
+            model.set_value(iter, SAMPLE_COLUMN_EDITABLE, state)
+            iter = model.iter_next(iter)
+        return True
+        
+    
+    def get_selected(self):
+        model = self.listview.get_model()
+        iter = model.get_iter_first()
+        items = []
+        while iter:
+            item = {}
+            sel = model.get_value(iter, SAMPLE_COLUMN_SELECTED)
+            if sel:
+                item['id'] = model.get_value(iter, SAMPLE_COLUMN_CODE)
+                item['name'] = model.get_value(iter, SAMPLE_COLUMN_NAME)
+                item['port'] = model.get_value(iter, SAMPLE_COLUMN_PORT)
+                items.append(item)
+            iter = model.iter_next(iter)
+        return items
 
-                                                                                                                                                                                    
+    def select_all(self, option=True):
+        model = self.listview.get_model()
+        iter = model.get_iter_first()
+        items = []
+        while iter:
+            model.set_value(iter, SAMPLE_COLUMN_SELECTED, option)
+            iter = model.iter_next(iter)
+        return
+
+    def clear(self):
+        model = self.listview.get_model()
+        model.clear()
+    
+    def export_excel(self, filename):
+        model = self.listview.get_model()
+        from pyExcelerator import Workbook
+        wb = Workbook()
+        wso = wb.add_sheet('0')
+        columns = ['container','port','id','name','comments']
+        for i in range(len(columns)):
+            wso.write(0, i, columns[i])
+        col = 1
+        iter = model.get_iter_first()
+        while iter:      
+            rowinfo = [model.get_value(iter, SAMPLE_COLUMN_CONTAINER),
+                       model.get_value(iter, SAMPLE_COLUMN_PORT),
+                       model.get_value(iter, SAMPLE_COLUMN_CODE),
+                       model.get_value(iter, SAMPLE_COLUMN_NAME),
+                       model.get_value(iter, SAMPLE_COLUMN_COMMENTS)]
+            for j in range(len(rowinfo)):
+                wso.write(col, j, rowinfo[j])
+            col +=1
+            iter = model.iter_next(iter)
+        wb.save(filename)
+
+    def export_csv(self, filename):
+        model = self.listview.get_model()
+        import csv
+        w = csv.writer(open(filename,'w'))
+        columns = ['container','port','id','name','comments']
+        w.writerow( columns)
+        iter = model.get_iter_first()
+        while iter:      
+            rowinfo = [model.get_value(iter, SAMPLE_COLUMN_CONTAINER),
+                       model.get_value(iter, SAMPLE_COLUMN_PORT),
+                       model.get_value(iter, SAMPLE_COLUMN_CODE),
+                       model.get_value(iter, SAMPLE_COLUMN_NAME),
+                       model.get_value(iter, SAMPLE_COLUMN_COMMENTS)]
+            w.writerow(rowinfo)
+            iter = model.iter_next(iter)
+
+    def import_csv(self, filename):
+        import csv
+        model = self.listview.get_model()
+        model.clear()
+        csvfile = open(filename)
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        reader = csv.reader(csvfile, dialect)
+        header = reader.next()
+        for row in reader:
+            item = {'state': 0, 'selected': False}
+            for i in range(len(header)):
+                item[header[i]] = row[i]
+            self.__add_item(item)
+        csvfile.close()
+            
+            
+        
 if __name__ == "__main__":
    
     win = gtk.Window()
--- mxdc/widgets/screeningmanager.py
+++ mxdc/widgets/screeningmanager.py
@@ -11,6 +11,40 @@ from bcm.beamline.mx import IBeamline
 from bcm.engine.scripting import get_scripts
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+( TASK_MOUNT,
+  TASK_ALIGN,
+  TASK_PAUSE,
+  TASK_COLLECT,
+  TASK_ANALYSE ) = range(5)
+
+TASKLET_NAME_MAP = {
+    TASK_MOUNT : 'Mount Crystal',
+    TASK_ALIGN : 'Align Crystal',
+    TASK_PAUSE : 'Pause',
+    TASK_COLLECT : 'Collect',
+    TASK_ANALYSE : 'Analyse',
+}
+
+(
+    QUEUE_COLUMN_DONE,
+    QUEUE_COLUMN_ID,
+    QUEUE_COLUMN_NAME,
+    QUEUE_COLUMN_TASK
+) = range(4)
+
+class Tasklet(object):
+    def __init__(self, task_type, default=False):
+        self.name = TASKLET_NAME_MAP[task_type]
+        self.options = {'enabled': default, 'default': default}      
+        self.task_type = task_type
+        
+    
+    def configure(self, **kwargs):
+        for k,v in kwargs.items():
+            self.options[k] = v
+
+    def __repr__(self):
+        return '<Tasklet: %s>' % self.name
 
 class ScreenManager(gtk.Frame):
     def __init__(self):
@@ -18,17 +52,35 @@ class ScreenManager(gtk.Frame):
         self.set_shadow_type(gtk.SHADOW_NONE)
         self._create_widgets()
         
-    def _create_widgets(self):
+    def _create_widgets(self):        
+        self.sample_list = SampleList()
+
         self._xml = gtk.glade.XML(os.path.join(DATA_DIR, 'screening_widget.glade'), 
-                                  'screening_widget')            
+                                  'screening_widget')
         self.screen_manager = self._xml.get_widget('screening_widget')
         self.sample_box = self._xml.get_widget('sample_box')
         self.video_book = self._xml.get_widget('video_book')
-        self.sample_list = SampleList()
-        self.sample_box.pack_start(self.sample_list, expand=True, fill=True)
-        self.sample_list.load_data(TEST_DATA)
-
+        self.task_config_box = self._xml.get_widget('task_config_box')
+        self.apply_btn = self._xml.get_widget('apply_btn')
+        self.reset_btn = self._xml.get_widget('reset_btn')
+        self.clear_btn = self._xml.get_widget('clear_btn')
+        self.select_all_btn = self._xml.get_widget('select_all_btn')
+        self.deselect_all_btn = self._xml.get_widget('deselect_all_btn')
+        self.task_queue_window = self._xml.get_widget('task_queue_window')
+        self.edit_tbtn = self._xml.get_widget('edit_tbtn')
+        
+        self.clear_btn.connect('clicked', self._on_queue_clear)
+        self.apply_btn.connect('clicked', self._on_sequence_apply)
+        self.reset_btn.connect('clicked', self._on_sequence_reset)
+        self.select_all_btn.connect('clicked', lambda x: self.sample_list.select_all(True) )
+        self.deselect_all_btn.connect('clicked', lambda x: self.sample_list.select_all(False) )
+        self.edit_tbtn.connect('toggled', self.sample_list.on_edit_toggled)
+        
         self.beamline = gsm.getUtility(IBeamline, 'bcm.beamline')      
+
+        self.sample_box.pack_start(self.sample_list, expand=True, fill=True)
+        self.sample_list.import_csv(os.path.join(DATA_DIR, 'test.csv')) 
+        #self.sample_list.load_data(TEST_DATA)
 
         # video        
         self.sample_viewer = SampleViewer()
@@ -37,7 +89,125 @@ class ScreenManager(gtk.Frame):
         self.video_book.append_page(self.hutch_viewer, tab_label=gtk.Label('Hutch Camera'))
         self.video_book.connect('map', lambda x: self.video_book.set_current_page(0))       
         
-        self.add(self.screen_manager)
-
+        # Task Configuration
+        self.TaskList = []
+        self.default_tasks = [ (TASK_MOUNT, True, True),
+                  (TASK_ALIGN, True, True),
+                  (TASK_PAUSE, False, False),
+                  (TASK_COLLECT, True, True),
+                  (TASK_COLLECT, True, False),
+                  (TASK_COLLECT, False, False),
+                  (TASK_ANALYSE, False, False),
+                  (TASK_PAUSE, False, False), ]
+        for key, sel, sen in self.default_tasks:
+            t = Tasklet(key, default=sel)
+            tbtn = gtk.CheckButton(t.name)
+            tbtn.set_active(sel)
+            tbtn.connect('toggled', self._on_task_toggle, t)
+            tbtn.set_sensitive(not(sen))
+            if key == TASK_COLLECT:
+                ctable = self._get_collect_setup(t)
+                ctable.attach(tbtn, 0,2,0,1)
+                self.task_config_box.pack_start(ctable, expand=True, fill=True)
+            else:
+                self.task_config_box.pack_start(tbtn, expand=True, fill=True)
+            self.TaskList.append( (t, tbtn) )
         
+        # Run List
+        self.listmodel = gtk.ListStore(
+            gobject.TYPE_BOOLEAN,
+            gobject.TYPE_STRING,
+            gobject.TYPE_STRING,
+            gobject.TYPE_PYOBJECT,
+        )
+        self.listview = gtk.TreeView(self.listmodel)
+        self.listview.set_rules_hint(True)
+        self._add_columns()
+        self.task_queue_window.add(self.listview)    
+
+        self.add(self.screen_manager) 
         self.show_all()
+    
+    def _get_collect_setup(self, task):
+        _xml2 = gtk.glade.XML(os.path.join(DATA_DIR, 'screening_widget.glade'), 
+                          'collect_settings')
+        tbl = _xml2.get_widget('collect_settings')
+        for key in ['angle','delta','time','frames']:
+            en = _xml2.get_widget('%s_entry' % key)
+            if key == 'frames':
+                en.default_value = int(en.get_text())
+            else:
+                en.default_value = float(en.get_text())
+            task.options[key] = en.default_value
+            en.connect('activate', self._on_settings_changed, None, task, key)
+            en.connect('focus-out-event', self._on_settings_changed, task, key)
+            en.set_alignment(1)
+        return tbl
+    
+    def _on_settings_changed(self, obj, event, task, key):
+        try:
+            val = float( obj.get_text() )
+        except:
+            if key == 'frames':
+                obj.set_text( '%d' % obj.default_value )
+            else:
+                obj.set_text( '%0.1f' % obj.default_value )
+            val = obj.default_value
+        task.options[key] = val
+        
+    def _add_item(self, item):
+        iter = self.listmodel.append()
+        self.listmodel.set(iter, 
+            QUEUE_COLUMN_DONE, item['done'], 
+            QUEUE_COLUMN_ID, item['id'],
+            QUEUE_COLUMN_NAME, item['name'],
+            QUEUE_COLUMN_TASK, item['task'],
+        )
+        
+    def _done_color(self, column, renderer, model, iter):
+        value = model.get_value(iter, QUEUE_COLUMN_DONE)
+        if value:
+            renderer.set_property("foreground", '#cc0000')
+        else:
+            renderer.set_property("foreground", None)
+        return
+
+    def _add_columns(self):
+        model = self.listview.get_model()
+                                          
+        # Name Column
+        renderer = gtk.CellRendererText()
+        column = gtk.TreeViewColumn('Crystal', renderer, text=QUEUE_COLUMN_ID)
+        column.set_cell_data_func(renderer, self._done_color)
+        self.listview.append_column(column)
+
+        # Task Column
+        renderer = gtk.CellRendererText()
+        column = gtk.TreeViewColumn('Task', renderer, text=QUEUE_COLUMN_NAME)
+        self.listview.append_column(column)
+    
+    def _on_task_toggle(self, obj, tasklet):
+        tasklet.configure(enabled=obj.get_active())
+    
+    def _on_sequence_apply(self, obj):
+        model = self.listview.get_model()
+        model.clear()
+        items = self.sample_list.get_selected()
+        for item in items:
+            for t,b in self.TaskList:
+                if t.options['enabled']:
+                    item['done'] = False
+                    item['name'] = t.name
+                    item['task'] = t
+                    self._add_item(item)
+    
+    def _on_sequence_reset(self, obj):
+        for t,b in self.TaskList:
+            b.set_active(t.options['default'])
+
+    def _on_queue_clear(self, obj):
+        model = self.listview.get_model()
+        model.clear()
+    
+    def _on_collect_setup(self, obj):
+        pass
