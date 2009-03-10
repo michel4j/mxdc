44a426dc201cc27aba929ccaa672a33ab7261044
michel <michel@OPI1608B-002.cs.clsi.ca>
2009-03-10 14:38:56 -0600

    Bug fix

---
+++ bcm/service/imagesync_server.py
@@ -0,0 +1,10 @@
+from twisted.application import internet, service
+from bcm.service.imagesync import ImgSyncService
+
+IMGSYNC_CONFIG = "/home/marccd/.imgsync.conf"
+IMGSYNC_SOURCE_LOG = "/home/marccd/log/stdouterr.log"
+
+application = service.Application('ImgConfig')
+f = ImgSyncService(config_file=IMGSYNC_CONFIG, log_file=IMGSYNC_SOURCE_LOG)
+serviceCollection = service.IServiceCollection(application)
+internet.TCPServer(8888, server.Site(resource.IResource(f))).setServiceParent(serviceCollection)
