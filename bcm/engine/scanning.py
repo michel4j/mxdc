89c70f966eb2911afd920780fa94a54cdcd86f61
Michel Fodje <michel.fodje@lightsource.ca>
2009-06-08 10:35:26 -0600

    modified:   bcm/engine/diffraction.py 	modified:   bcm/engine/scanning.py 	new file:   etc/08b1-console.conf 	modified:   mxdc/widgets/collectmanager.py 	modified:   mxdc/widgets/hutchmanager.py 	modified:   mxdc/widgets/imageviewer.py 	modified:   mxdc/widgets/misc.py 	modified:   mxdc/widgets/plotter.py 	modified:   mxdc/widgets/statuspanel.py

--- bcm/engine/scanning.py
+++ bcm/engine/scanning.py
@@ -6,6 +6,7 @@ import gc
 
 from zope.interface import Interface, Attribute
 from zope.interface import implements
+from twisted.python.components import globalRegistry
 from bcm.protocol import ca
 from bcm.utils.log import get_module_logger
 from bcm.device.interfaces import IMotor, ICounter
@@ -16,6 +17,31 @@ _logger = get_module_logger(__name__)
 class ScanError(Exception):
     """Scan Error."""
 
+class IScanPlotter(Interface):
+    """Scan Plotter Object."""
+    
+    def connect_scan(self, scanner):
+        """Connect handlers to scanner."""
+        
+    def on_start(scan, data):
+        """Clear Scan and setup based on contents of info dictionary."""       
+    
+    def on_progress(scan, data):
+        """Progress handler."""
+
+    def on_new_point(scan, data):
+        """New point handler."""
+    
+    def on_done(scan):
+        """Done handler."""
+    
+    def on_stop(scan):
+        """Stop handler."""
+    
+    def on_error(scan, error):
+        """Error handler."""
+        
+        
 class IScan(Interface):
     """Scan object."""
     
@@ -54,6 +80,12 @@ class BasicScan(gobject.GObject):
         self._stopped = False
         self.data = []
         self.data_names = []
+        try:
+            self.plotter = globalRegistry.lookup([], IScanPlotter)
+            self.plotter.connect_scanner(self)
+        except:
+            self.plotter = None
+            _logger.info('No Plotter found.')
                 
     def configure(self, **kwargs):
         self.data = []
@@ -81,7 +113,7 @@ class BasicScan(gobject.GObject):
         except:
             self._logger.error("Could not open file '%s' for writing" % filename)
             return
-        f.write('# Scan Type: %s' % (self.__class__.__name__))
+        f.write('# Scan Type: %s\n' % (self.__class__.__name__))
         f.write('#')
         for name in self.data_names:
             f.write('%15s' % name)
@@ -252,4 +284,70 @@ class RelScan2(AbsScan2):
         self._start_pos2 = cur_pos2 + start_offset2
         self._end_pos2 = cur_pos2 + end_offset2 
         
+class GridScan(BasicScan):
+    """An object which performs an absolute scan of two motors in a grid."""
+    
+    def __init__(self, mtr1, start_pos1, end_pos1, mtr2, start_pos2, end_pos2, steps, cntr, t, i0=None):
+        BasicScan.__init__(self)
+        self.configure(mtr1, start_pos1, end_pos1, mtr2, start_pos2, end_pos2, steps, cntr, t, i0)
+        
+    def configure(self, mtr1, start_pos1, end_pos1, mtr2, start_pos2, end_pos2, steps, cntr, t, i0=None):
+        self._motor1 = IMotor(mtr1)
+        self._motor2 = IMotor(mtr2)
+        self._counter = ICounter(cntr)
+        if i0 is not None:
+            self._i0 = ICounter(i0)
+        else:
+            self._i0 = None
+        self._duration = t
+        self._steps = steps
+        self._start_pos1 = start_pos1
+        self._end_pos1 = end_pos1 
+        self._start_pos2 = start_pos2
+        self._end_pos2 = end_pos2 
+        
+     
+    def run(self):
+        step_size1 = (self._end_pos1 - self._start_pos1) / float( self._steps )
+        step_size2 = (self._end_pos2 - self._start_pos2) / float( self._steps )
+        self.data_names = [self._motor1.name,
+                           self._motor2.name,
+                           self._counter.name+'_scaled', 
+                           'I_0',
+                           self._counter.name]
+        _logger.info("Scanning '%s':'%s' vs '%s' " % (self._motor1.name,
+                                                      self._motor2.name,
+                                                      self._counter.name))
+        _logger.info("%4s '%13s' '%13s' '%13s_normalized' '%13s' '%13s'" % ('#',
+                                                   self._motor1.name,
+                                                   self._motor2.name,
+                                                   self._counter.name,
+                                                   'I_0',
+                                                   self._counter.name))
+        for i in xrange(self._steps*self._steps):
+            if self._stopped:
+                _logger.info( "Scan stopped!" )
+                break
+            i_1 = i % self._steps
+            i_2 = i // self._steps
+            x1 = self._start_pos1 + (i_1 * step_size1)
+            x2 = self._start_pos2 + (i_2 * step_size2)
+            self._motor1.move_to(x1)
+            self._motor1.wait()
+            self._motor2.move_to(x2)
+            self._motor2.wait()
+            y = self._counter.count(self._duration)         
+            if self._i0 is not None:         
+                i0 = self._i0.count(self._duration)
+            else:
+                i0 = 1.0
+            self.data.append( [x1, x2, y/i0, i0, y] )
+            _logger.info("%4d %15g %15g %15g %15g %15g" % (i, x1, x2, y/i0, i0, y))
+            gobject.idle_add(self.emit, "new-point", (x1, x2, y/i0, i0, y) )
+            gobject.idle_add(self.emit, "progress", (i + 1.0)/(self._steps + 1.0) )
+             
+        gobject.idle_add(self.emit, "done")
+
+
+
 gobject.type_register(BasicScan)
