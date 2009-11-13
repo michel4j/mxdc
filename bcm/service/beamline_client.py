97ad95d9f78663b1b1e84a974d021e7d8f5f1e7a
Michel Fodje <michel.fodje@lightsource.ca>
2009-11-13 15:13:42 -0600

    bugfix

--- bcm/service/beamline_client.py
+++ bcm/service/beamline_client.py
@@ -1,9 +1,10 @@
+import gobject, gtk
 from twisted.internet import glib2reactor
 glib2reactor.install()
 
 from zope.interface import providedBy, implements
 from twisted.spread import pb, interfaces
-from twisted.internet import reactor
+from twisted.internet import reactor, defer, threads
 from twisted.python import log
 from twisted.python.components import globalRegistry
 from bcm.device.remote import *
@@ -11,49 +12,45 @@ from bcm.beamline.interfaces import IBeamline
 import sys, os
 import re
 
-class BeamlineClient(object):
+class BeamlineClient(gobject.GObject):
     implements(IBeamline)
     
     def __init__(self):
+        gobject.GObject.__init__(self)
         self.registry = {}
-        self._dev_count = 0
         self.ready = False
 
     def setup(self, remote):
         self.remote = remote
         remote.callRemote("getConfig").addCallbacks(self._got_config, log.err)
+        return threads.deferToThread(self._wait_ready)
+    
+    def _wait_ready(self):
+        while not self.ready:
+            time.sleep(0.1)
         
     def _got_device(self, response, name):
         device = registry.queryAdapter(response[1], IDeviceClient, response[0])
-        print 'Got', device
         self.registry[name] = device
-        self._devs += 1
-        print 'Setting up %d of %d devices' % (self._devs, self._dev_count)
-        if self._devs == self._dev_count:
-            globalRegistry.register([], IBeamline, '', self)
-            print 'Beamline Registered'
-            self.ready = True
+        print 'Setting up %s' % (name)
 
+    def _beamline_ready(self, _ ):
+        globalRegistry.register([], IBeamline, '', self)
+        print 'Beamline Registered'
+        self.ready = True
+        
     
     def _got_config(self, response):
-        self._devs =0
-        import pprint        
-        pprint.pprint(response)
-        
+        dev_reqs = []
         for section, dev_name, cmd in response:
             dev_type = cmd.split('(')[0]
             if dev_type in ['PseudoMotor','VMEMotor','BraggEnergyMotor','Positioner','Attenuator']:
-                self._dev_count += 1
                 d = self.remote.callRemote('getDevice', dev_name)
                 d.addCallback(self._got_device, dev_name).addErrback(log.err)
-#            else:
-#                n_cmd = re.sub("'@([^- ,]+)'", "self.registry['\\1']", cmd)
-#                reg_cmd = "self.registry['%s'] = %s" % (dev_name, n_cmd)
-#                exec(reg_cmd)
-#                if section in ['utilities', 'services']:
-#                    util_cmd = "self.%s = self.registry['%s']" % (dev_name, dev_name)
-#                    exec(util_cmd)
+                dev_reqs.append(d)
         
+        dl = defer.DeferredList(dev_reqs)
+        dl.addCallback(self._beamline_ready)
 
     def __getitem__(self, key):
         try:
@@ -71,12 +68,15 @@ class BeamlineClient(object):
         except AttributeError:
             return self.registry[key]
 
+def test(_):
+    print 'everything is ready'
+    
 def main():
     beamline = BeamlineClient()
     factory = pb.PBClientFactory()
     reactor.connectTCP("localhost", 8880, factory)
     deferred = factory.getRootObject()
-    deferred.addCallback(beamline.setup)
+    deferred.addCallback(beamline.setup).addCallback(test)
     reactor.run()
 
 if __name__ == '__main__':
