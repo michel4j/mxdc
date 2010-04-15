b36c18c6aaee32f68eca3ba7d625ecd5d7874137
michel <michel@localhost.localdomain>
2010-04-15 10:05:15 -0600

    Updated config files to match new host names, and MX300 installation at 08ID1

--- bcm/service/imagesync_server.py
+++ bcm/service/imagesync_server.py
@@ -1,7 +1,6 @@
 import os, sys
 from twisted.application import internet, service
 from twisted.web import resource, server, static
-sys.path.append(os.path.dirname(__file__))
 
 from imagesync import ImgSyncService
 
