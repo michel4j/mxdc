e543717a50b18b7aeffff0d1ad6627b26b843bd6
michel <michel@localhost.localdomain>
2010-05-18 15:59:25 -0600

    new splash screen, changes to dewar loader

--- mxdc/widgets/splash.py
+++ mxdc/widgets/splash.py
@@ -2,6 +2,7 @@ import sys, os
 import time
 import logging
 import pango
+import cairo
 import gtk, gobject
 
 from mxdc.widgets.misc import LinearProgress
@@ -19,7 +20,7 @@ class LabelHandler(logging.Handler):
         #while gtk.events_pending():
         #    gtk.main_iteration()
 
-class Splash(object):
+class Splash1(object):
     def __init__(self, version, color=None, bg=None):
         image_file = os.path.join(DATA_DIR, 'cool-splash.png')
         self.version = version
@@ -61,4 +62,61 @@ class Splash(object):
         self.canvas.window.draw_layout(self.style.bg_gc[gtk.STATE_NORMAL], 20, self.height/2 + 40, self.log)
         
 
+class Splash(gtk.Window):
+    def __init__(self, version, image='cool-splash.png'):
+        gtk.Window.__init__(self)
+        self.img_file = os.path.join(DATA_DIR, image)
+        self.version = version
+        pixbuf = gtk.gdk.pixbuf_new_from_file(self.img_file)
+        self.width, self.height = pixbuf.get_width(), pixbuf.get_height()
+        self.set_size_request(self.width, self.height)
+        self.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_SPLASHSCREEN)
+        self.set_gravity(gtk.gdk.GRAVITY_CENTER)
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.connect('expose-event', self.on_expose)
+        self.connect('screen-changed', self.on_set_screen)
+        self.on_set_screen(self)
+        self.set_app_paintable(True)
+    
+    def on_set_screen(self, widget, old_screen=None):
+        # To check if the display supports alpha channels, get the colormap
+        screen = widget.get_screen()
+        colormap = screen.get_rgba_colormap()
+        if colormap == None:
+            colormap = screen.get_rgb_colormap()
+            self.supports_alpha = False
+        else:
+            self.supports_alpha = True
+    
+        # Now we have a colormap appropriate for the screen, use it
+        widget.set_colormap(colormap)
+    
+    def on_expose(self, widget, event):
+        cr = widget.window.cairo_create()
+                
+        if self.supports_alpha:
+            cr.set_source_rgba(1.0, 1.0, 1.0, 0.0) # Transparent
+        else:
+            cr.set_source_rgb(1.0, 1.0, 1.0) # Opaque white
+            
+        # Draw the background
+        cr.set_operator(cairo.OPERATOR_SOURCE)
+        cr.paint()
+        pixbuf = gtk.gdk.pixbuf_new_from_file(self.img_file)
+        pix, mask = pixbuf.render_pixmap_and_mask(230)
+        cr = widget.window.cairo_create()
+        cr.set_source_pixbuf(pixbuf,0,0)
+        cr.paint()
+        cr.set_source_rgb(0.3,0.2, 0.1)
+        cr.set_line_width(0.5)
+        cr.move_to(28, 28+self.height/2)
+        cr.select_font_face("Luxi Sans",
+                cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
 
+        cr.set_font_size(14)
+        cr.text_path('Release %s' % self.version)
+        cr.fill()
+        #cr.stroke()
+        #widget.shape_combine_mask(mask, 0, 0)
+        
+        return False
