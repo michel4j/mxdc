516ac7ef37f19b26b5c241c87532f214fef87675
Michel Fodje <michel.fodje@lightsource.ca>
2010-12-07 15:29:34 -0600

    configuration improvements for 08ID-1

--- bcm/scripts/mode_changes.py
+++ bcm/scripts/mode_changes.py
@@ -4,18 +4,17 @@ class SetMountMode(Script):
     description = "Prepare for sample mounting."
     def run(self):
         safe_distance = 700
-        safe_beamstop = 80
+        safe_beamstop = self.beamline.config['safe_beamstop']
         self.beamline.distance.move_to(safe_distance)
         self.beamline.goniometer.set_mode('MOUNTING', wait=True)
         self.beamline.beamstop_z.move_to(safe_beamstop)
         self.beamline.beamstop_z.wait()
-        self.beamline.distance.wait()
         
         
 class SetCenteringMode(Script):
     description = "Prepare for crystal centering."
     def run(self):
-        safe_beamstop = 50
+        safe_beamstop = self.beamline.config['default_beamstop']
         save_distance = 300
         self.beamline.goniometer.set_mode('CENTERING', wait=True)
         self.beamline.beamstop_z.move_to(safe_beamstop)
@@ -26,6 +25,8 @@ class SetCollectMode(Script):
     description = "Prepare for data collection."
     def run(self):
         self.beamline.goniometer.set_mode('COLLECT', wait=True)
+        beamstop_pos = self.beamline.config['default_beamstop']
+        self.beamline.beamstop_z.move_to(beamstop_pos)
         
 myscript0 = SetCenteringMode()
 myscript1 = SetMountMode()
