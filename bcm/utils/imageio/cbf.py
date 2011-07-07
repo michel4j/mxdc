ddd0365cfbd18672771efcf917d307d7ce21fc5a
Michel Fodje <michel.fodje@lightsource.ca>
2011-07-07 11:13:08 -0600

    - bug fix: get_max_int should return a double not an int, cbf.py

--- bcm/utils/imageio/cbf.py
+++ bcm/utils/imageio/cbf.py
@@ -144,9 +144,9 @@ cbflib.cbf_get_overload.argtypes = [c_void_p, c_uint, POINTER(c_double)]
 def get_max_int(t):
     v = t(2**(8*sizeof(t))-1).value
     if v == -1: #signed
-        return t(2**(8*sizeof(t)-1)-1)
+        return c_double(2**(8*sizeof(t)-1)-1)
     else:       #unsiged
-        return t(2**(8*sizeof(t)-1))
+        return c_double(2**(8*sizeof(t)-1))
     
 class CBFImageFile(object):
     def __init__(self, filename, header_only=False):
