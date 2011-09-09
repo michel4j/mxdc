62d8ee2c43848777d5a51a6f8a9017c2fbb778db
Michel Fodje <michel.fodje@lightsource.ca>
2011-09-09 12:31:20 -0600

    - bug fixes to prevent automounter crashing into backlight again

--- bcm/scripts/mode_changes.py
+++ bcm/scripts/mode_changes.py
@@ -3,45 +3,50 @@ from bcm.engine.scripting import Script
 class SetMountMode(Script):
     description = "Prepare for manual sample mounting."
     def run(self):
-        safe_distance = 700
-        safe_beamstop = self.beamline.config['safe_beamstop']
-        self.beamline.detector_z.move_to(safe_distance)
-        self.beamline.goniometer.set_mode('MOUNTING', wait=True)
-        self.beamline.beamstop_z.move_to(safe_beamstop)
-        self.beamline.cryojet.nozzle.open()
-        self.beamline.beamstop_z.wait()
+        if not self.beamline.automounter.is_busy():
+            safe_distance = 700
+            if self.beamline.detector_z.get_position() < safe_distance:
+                self.beamline.detector_z.move_to(safe_distance)
+            safe_beamstop = self.beamline.config['safe_beamstop']
+            self.beamline.goniometer.set_mode('MOUNTING', wait=True)
+            self.beamline.beamstop_z.move_to(safe_beamstop)
+            self.beamline.cryojet.nozzle.open()
+            self.beamline.beamstop_z.wait()
         
         
         
 class SetCenteringMode(Script):
     description = "Prepare for crystal centering."
     def run(self):
-        safe_beamstop = self.beamline.config['default_beamstop']
-        safe_distance = 300
-        
-        mount_distance = 700 # Should be the same as the 'safe_distance' from SetMountMode
-        
-        self.beamline.goniometer.set_mode('CENTERING', wait=True)
-        self.beamline.beamstop_z.move_to(safe_beamstop)
-        if abs(self.beamline.detector_z.get_position() - mount_distance) < 1:
-            self.beamline.detector_z.move_to(safe_distance)
-        self.beamline.cryojet.nozzle.close()
+        if not self.beamline.automounter.is_busy():
+            safe_beamstop = self.beamline.config['default_beamstop']
+            safe_distance = 300
+            
+            mount_distance = 700 # Should be the same as the 'safe_distance' from SetMountMode
+            
+            self.beamline.goniometer.set_mode('CENTERING', wait=True)
+            self.beamline.beamstop_z.move_to(safe_beamstop)
+            if abs(self.beamline.detector_z.get_position() - mount_distance) < 1:
+                self.beamline.detector_z.move_to(safe_distance)
+            self.beamline.cryojet.nozzle.close()
         
 
 class SetCollectMode(Script):
     description = "Prepare for data collection."
     def run(self):
-        self.beamline.goniometer.set_mode('COLLECT', wait=True)
-        beamstop_pos = self.beamline.config['default_beamstop']
-        self.beamline.beamstop_z.move_to(beamstop_pos)
-        self.beamline.cryojet.nozzle.close()
+        if not self.beamline.automounter.is_busy():
+            self.beamline.goniometer.set_mode('COLLECT', wait=True)
+            beamstop_pos = self.beamline.config['default_beamstop']
+            self.beamline.beamstop_z.move_to(beamstop_pos)
+            self.beamline.cryojet.nozzle.close()
 
 class SetBeamMode(Script):
     description = "Switch to Beam Inspection Mode."
     def run(self):
-        self.beamline.goniometer.set_mode('BEAM', wait=True)
-        #FIXME: should we open the shutter here? Who is responsible for
-        # closing it
+        if not self.beamline.automounter.is_busy():
+            self.beamline.goniometer.set_mode('BEAM', wait=True)
+            #FIXME: should we open the shutter here? Who is responsible for
+            # closing it
 
         
 myscript0 = SetCenteringMode()
