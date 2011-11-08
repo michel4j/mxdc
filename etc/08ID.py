67009f8196409e86fd55c3f91bc93651d6bc51ee
Michel Fodje <michel.fodje@lightsource.ca>
2011-11-08 16:45:14 -0600

    - bugfix: scanmanager matplotlib linewidth not compatible with mpl 0.99

--- etc/08ID.py
+++ etc/08ID.py
@@ -77,7 +77,7 @@ DEVICES = {
     
     # Misc: Automounter, HC1 etc
     'automounter':  Automounter('ROB16083I', 'ROB1608-300'),
-    'humidifier': HumidityController('HC1608-01'),
+    #'humidifier': HumidityController('HC1608-01'),
     'attenuator': Attenuator('PFIL1608-3-I10-01', 'BL08ID1:energy'),
     'mca': MultiChannelAnalyzer('XFD1608-101:mca1'),
 }
