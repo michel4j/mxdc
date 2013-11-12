a11e12c9d86b31932cee87e06dba1a51da9c04ea
Michel Fodje <michel4j@gmail.com>
2013-11-12 14:35:24 -0600

    - Remote shutdown feature for admins

--- mxdc/mxdcapp.py
+++ mxdc/mxdcapp.py
@@ -1,75 +1,116 @@
 from twisted.internet import gtk2reactor
 gtk2reactor.install()
 
+from bcm.service import mxdctools
 from bcm.beamline.mx import MXBeamline
-from bcm.beamline.remote import BeamlineClient
 from bcm.utils import mdns
 from bcm.utils.log import get_module_logger, log_to_console
 from bcm.utils.misc import get_project_name
 from mxdc.AppWindow import AppWindow
-from mxdc.widgets.dialogs import error
+from mxdc.widgets import dialogs
 from mxdc.utils import excepthook
 from twisted.internet import reactor
 from twisted.spread import pb
 import os
 import time
 import warnings
+import gobject
+import gtk
+
+MXDC_PORT = 9999
+SERVICE_DATA = {
+    'user': get_project_name(), 
+    'started': time.asctime(time.localtime()),
+    'beamline': os.environ.get('BCM_BEAMLINE', 'SIM')
+}
 
 warnings.simplefilter("ignore")
 excepthook.install()
 _logger = get_module_logger('mxdc')
 
 class MXDCApp(object):
-    def provider_success(self):
-        _ = MXBeamline()
-        self.main_window.connect('destroy', self.do_quit)
-        self.main_window.run()
-
-    def provider_failure(self):
-        _logger.error('An instance of MXDC is already running on the local network. Only one instance permitted.')
-        error('MXDC Already Running', 'An instance of MXDC is already running on the local network. Only one instance permitted.')
-        self.do_quit()
-
+    def __init__(self):
+        self.remote_mxdc = None
+        
     def run_local(self):
         self.main_window = AppWindow()
-        _service_data = {'user': get_project_name(), 
-                         'started': time.asctime(time.localtime()),
-                         'beamline': os.environ.get('BCM_BEAMLINE', 'SIM')}
-
-        if _service_data['beamline'] == 'SIM':
+        self.beamline = MXBeamline()
+        self.main_window.connect('destroy', self.do_quit)
+        if SERVICE_DATA['beamline'] == 'SIM':
             self.provider_success()
-            return False
-        
+            return False       
         try:
             self.browser = mdns.Browser('_mxdc._tcp')
-            self.browser.connect('added', self.found_existing)
-            self.provider = mdns.Provider('MXDC Client (%s)' % os.environ.get('BCM_BEAMLINE', 'SIM'), '_mxdc._tcp', 9999, _service_data, unique=True)
-            self.provider.connect('running', lambda x: self.provider_success())
-            self.provider.connect('collision', lambda x: self.provider_failure())
-
+            self.browser.connect('added', self.service_found)
+            self.browser.connect('removed', self.service_removed)
+            gobject.timeout_add(2000, self.broadcast_service) # broadcast after a short while
+            self.main_window.run()
         except mdns.mDNSError:
             self.provider_failure()
         return False
     
-    def found_existing(self, obj, instance):
-        data = instance['data']
-        if data.get('beamline') == os.environ.get('BCM_BEAMLINE', 'sim'):
-            _logger.info('MxDC running on `%s`, by `%s` since `%s` .' % (instance['host'], data['user'], data['started'] ))    
+    def broadcast_service(self):
+        if self.remote_mxdc is None:
+            self.provider = mdns.Provider('MXDC Client (%s)' % os.environ.get('BCM_BEAMLINE', 'SIM'), '_mxdc._tcp', MXDC_PORT, SERVICE_DATA, unique=True)
+            self.provider.connect('collision', lambda x: self.provider_failure())
+            self.service = mxdctools.MXDCService(None)       
+            reactor.listenTCP(MXDC_PORT, pb.PBServerFactory(mxdctools.IPerspectiveMXDC(self.service)))
+        else:
+            self.provider_failure()
         
+    def service_found(self, obj, instance):
+        self._service_data = instance
+        data = self._service_data['data']
+        _logger.info('MxDC running on `%s`, by `%s` since `%s` .' % (instance['host'], data['user'], data['started'] ))    
+        self.factory = pb.PBClientFactory()
+        self.factory.getRootObject().addCallbacks(self._remote_mxdc_success, self._remote_mxdc_failed)
+        reactor.connectTCP(self._service_data['address'],
+                           self._service_data['port'], self.factory)
         
-    def run_remote(self):
-        self.main_window = AppWindow()
-        beamline = BeamlineClient()
-        self.main_window.connect('destroy', self.do_quit)
-        beamline.connect('ready', lambda x, y: self.main_window.run())
-        factory = pb.PBClientFactory()
-        reactor.connectTCP("localhost", 8880, factory)
-        deferred = factory.getRootObject()
-        deferred.addCallback(beamline.setup)
-    
+    def service_removed(self, obj, instance):
+        self.remote_mxdc = None
+            
     def do_quit(self, obj=None):
         _logger.info('Stopping...')
         reactor.stop()
+
+    def provider_success(self):
+        pass
+
+    def provider_failure(self):
+        msg = 'On <i>%s</i>, by user <i>%s</i> since <i>%s</i>. Only one instance permitted!' % (self._service_data['host'], 
+                        self._service_data['data']['user'], self._service_data['data']['started'])
+        if self.beamline.config['admin_group'] in os.getgroups():
+            msg += '\n\nDo you want to shut it down?'
+            response = dialogs.yesno('MXDC Already Running', msg)
+            print response
+            if response == gtk.RESPONSE_YES and self.remote_mxdc is not None:
+                self.remote_mxdc.callRemote('shutdown')
+                gobject.timeout_add(2000, self.broadcast_service) # broadcast after a short while
+            else:
+                self.do_quit()
+        else:
+            dialogs.error('MXDC Already Running', msg)
+            self.do_quit()
+    
+    def _rshutdown_success(self):
+        self.provider = mdns.Provider('MXDC Client (%s)' % os.environ.get('BCM_BEAMLINE', 'SIM'), '_mxdc._tcp', MXDC_PORT, SERVICE_DATA, unique=True)
+    
+    def _rshutdown_failure(self, failure):
+        r = failure.trap(pb.PBConnectionLost)
+        if r == pb.PBConnectionLost:
+            self._rshutdown_success()
+        else:
+            _logger.error('Remote Shutdown Failed')
+            self.do_quit()
+        
+    def _remote_mxdc_success(self, perspective):
+        self.remote_mxdc = perspective
+    
+    def _remote_mxdc_failed(self, failure):
+        _logger.error('An instance of MXDC is already running on the local network. Only one instance permitted.')
+        dialogs.error('MXDC Already Running', 'An instance of MXDC is already running on the local network. Only one instance permitted.')
+        self.do_quit()
         
 def main():
     try:
@@ -82,7 +123,6 @@ def main():
         
     app = MXDCApp()
     app.run_local()
-    #app.run_remote()
 
 if __name__ == "__main__":
     log_to_console()
