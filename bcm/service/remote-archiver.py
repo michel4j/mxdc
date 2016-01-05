db27e144d1944b5a0e8a90bc8192afcb53566646
Michel Fodje <michel.fodje@lightsource.ca>
2016-01-05 15:15:46 -0600

    - remote archiver

--- bcm/service/remote-archiver.py
+++ bcm/service/remote-archiver.py
@@ -8,9 +8,11 @@ import sys
 import time
 import re
 import pty
+from datetime import datetime
+import threading
 
 SYNC_TIME = 2*60 # 2 minutes
-
+TIME_FORMAT = "%Y-%m-%dT%H:%M"
 def _call_and_peek_output(cmd, shell=False):
     master, slave = pty.openpty()
     p = subprocess.Popen(cmd, shell=shell, stdin=None, stdout=slave, close_fds=True)
@@ -57,8 +59,9 @@ class RsyncApp(object):
     def __init__(self, src, dest):
         self.src = src.strip()
         self.dest = dest
+        self.port = 22
         self.tgt = os.path.join(self.dest, os.path.basename(self.src))
-        
+        self.proceed = True
         #remove trailing slash from source
         if self.src.endswith(os.sep):
             self.src = self.src[:-1]
@@ -86,25 +89,23 @@ class RsyncApp(object):
         return tgt_sz, dst_avl, dst_pct
         
     def run(self):
-        command = 'rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --modify-window=2 --progress --exclude "*:*" %s %s' % (re.escape(self.src), re.escape(self.dest))
+        command = 'rsync -avz -e "ssh -p %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --modify-window=2 --progress --exclude "*:*" %s %s' % (self.port, self.src, re.escape(self.dest))
+        if not os.path.exists(self.dest):
+            os.makedirs(self.dest)
         if os.access(self.dest, os.W_OK):
             args = shlex.split(command)
             call_and_print_output(args)
-            return True
+            self.proceed = True
         else:
             return False
 
-def main():
-    if len(sys.argv) == 3:
-        sync = RsyncApp(sys.argv[1], sys.argv[2])
-        proceed = True
+def main(sync):
         timeout = SYNC_TIME
-        target_dir = os.path.join(sync.dest, os.path.basename(sync.src))       
-        
-        while proceed:
-            proceed = sync.run()
+        target_dir = os.path.join(sync.dest, sync.src.split(':')[-1])
+        while sync.proceed:
+            sync.run()
             timeout = SYNC_TIME
-            if not proceed:
+            if not sync.proceed:
                 print 'ERROR: Source or Destination not accessible. Synchronization terminating.'
                 break
             while timeout > 0:
@@ -118,15 +119,35 @@ def main():
                 print 'Next synchronization in %d minute(s)' % (timeout//60)
                 timeout -= 60
                 time.sleep(60)
-    else:
-        print "usage: archiver <source directory>  <destination directory>"
-    
+
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print "Archiver stopped"
-        sys.exit(0)
-    except subprocess.CalledProcessError:
-        print "Transfer incomplete"
+    if len(sys.argv) >= 3:
+        sync = RsyncApp(sys.argv[1], sys.argv[2])
+        if len(sys.argv) > 3:
+            sync.port = sys.argv[3]
+        if len(sys.argv) > 4:
+            start_time = datetime.strptime(sys.argv[4], TIME_FORMAT)
+            delay = True
+        else:
+            start_time = datetime.now()
+            delay = False
+        now = datetime.now()
+        run_after = (start_time - now).total_seconds()
+        thread = None
+        try:
+            if delay:
+                print "Remote Syncronization will start on %s" % start_time.isoformat()
+                thread = threading.Timer(run_after, main, args=[sync])
+                thread.start()
+            else:
+                print "Remote Syncronization Started ..."
+                main(sync)
+
+        except KeyboardInterrupt:
+            print "Archiver stopped"
+            sys.exit(0)
+        except subprocess.CalledProcessError:
+            print "Transfer incomplete"
+    else:
+        print "usage: archiver <username@hostname:source directory>  <destination directory> [<portno>] [<start_time, format YYYY-MM-DD>]"
