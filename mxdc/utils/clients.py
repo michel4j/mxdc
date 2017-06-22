dfdaa430e1266a6fe4b8c68048b11ed26ad76867
Michel Fodje <michel.fodje@lightsource.ca>
2017-06-22 09:33:32 -0600

    Importing more changes from version 5

--- mxdc/utils/clients.py
+++ mxdc/utils/clients.py
@@ -1,25 +1,25 @@
-'''
-Created on Oct 28, 2010
-
-@author: michel
-'''
-
-from twisted.spread import pb
-from twisted.internet import reactor
-
-from mxdc.utils import mdns, misc
-from mxdc.utils.log import get_module_logger
-from mxdc.service.base import BaseService
-
-import requests
-
-import re
-from dpm.service import common
-from gi.repository import GObject
-import os
 import json
+import os
+import re
+
+import gobject
+import requests
+from mxdc.service.base import BaseService
+from mxdc.utils import mdns
+from mxdc.utils.misc import get_project_name
+from mxdc.utils.log import get_module_logger
+from twisted.internet import reactor, error
+from twisted.spread import pb
 
 _logger = get_module_logger(__name__)
+
+
+# For some reason clientConnectionMade is not called
+class ServerClientFactory(pb.PBClientFactory):
+    def buildProtocol(self, addr):
+        broker = pb.PBClientFactory.buildProtocol(self, addr)
+        pb.PBClientFactory.clientConnectionMade(self, broker)
+        return broker
 
 
 class DPMClient(BaseService):
@@ -27,6 +27,7 @@ class DPMClient(BaseService):
         BaseService.__init__(self)
         self.name = "AutoProcess Service"
         self._service_found = False
+        self._service_data = {}
         self._ready = False
         if address is not None:
             m = re.match('([\w.\-_]+):(\d+)', address)
@@ -36,72 +37,58 @@ class DPMClient(BaseService):
                         'address': m.group(1),
                         'port': int(m.group(2)),
                         }
-            self.on_dpm_service_added(None, data)
+                self.on_service_added(None, data)
         else:
-            GObject.idle_add(self.setup)
+            gobject.idle_add(self.setup)
 
-    def on_dpm_service_added(self, obj, data):
+    def on_service_added(self, obj, data):
         if self._service_found:
             return
         self._service_found = True
         self._service_data = data
-        _logger.info('AutoProcess Service found at %s:%s' % (self._service_data['host'],
-                                                             self._service_data['port']))
-        self.factory = pb.PBClientFactory()
-        self.factory.getRootObject().addCallback(self.on_dpm_connected).addErrback(self.dump_error)
-        reactor.connectTCP(self._service_data['address'],
-                           self._service_data['port'], self.factory)
+        self.factory = ServerClientFactory()  # pb.PBClientFactory()
+        self.factory.getRootObject().addCallback(self.on_connected).addErrback(self.on_connection_failed)
+        reactor.connectTCP(self._service_data['address'], self._service_data['port'], self.factory)
+        _logger.info(
+            'AutoProcess Service found at {}:{}'.format(self._service_data['host'], self._service_data['port'])
+        )
 
-    def on_dpm_service_removed(self, obj, data):
+    def on_service_removed(self, obj, data):
         if not self._service_found and self._service_data['host'] == data['host']:
             return
         self._service_found = False
         self._ready = False
-        _logger.warning('AutoProcess Service %s:%s disconnected.' % (self._service_data['host'],
-                                                                     self._service_data['port']))
+        _logger.warning(
+            'AutoProcess Service {}:{} disconnected.'.format(self._service_data['host'], self._service_data['port'])
+        )
         self.set_state(active=False)
 
     def setup(self):
         """Find out the connection details of the AutoProcess Server using mdns
         and initiate a connection"""
-        import time
-        _service_data = {'user': os.getlogin(),
-                         'uid': os.getuid(),
-                         'gid': os.getgid(),
-                         'started': time.asctime(time.localtime())}
         self.browser = mdns.Browser('_cmcf_dpm._tcp')
-        self.browser.connect('added', self.on_dpm_service_added)
-        self.browser.connect('removed', self.on_dpm_service_removed)
+        self.browser.connect('added', self.on_service_added)
+        self.browser.connect('removed', self.on_service_removed)
 
-    def on_dpm_connected(self, perspective):
+    def on_connected(self, perspective):
         """ I am called when a connection to the AutoProcess Server has been established.
         I expect to receive a remote perspective which will be used to call remote methods
         on the DPM server."""
         _logger.info('Connection to AutoProcess Server established')
         self.service = perspective
-        self.service.notifyOnDisconnect(self._disconnect_cb)
+        self.service.notifyOnDisconnect(self.on_disconnected)
         self._ready = True
         self.set_state(active=True)
 
-    def _disconnect_cb(self, obj):
+    def on_disconnected(self, obj):
         """Used to detect disconnections if MDNS is not being used."""
         self.set_state(active=False)
 
     def on_connection_failed(self, reason):
-        _logger.error('Could not connect to AutoProcess Server: %', reason)
+        _logger.error('Connection to AutoProcess Server Failed')
 
     def is_ready(self):
         return self._ready
-
-    def dump_results(self, data):
-        """pretty print the data received from the server"""
-        import pprint
-        pp = pprint.PrettyPrinter(indent=4, depth=4)
-        _logger.info('Server sent: %s' % pp.pformat(data))
-
-    def dump_error(self, failure):
-        r = failure.trap(common.InvalidUser, common.CommandFailed)
-        _logger.error('<%s -- %s>.' % (r, failure.getErrorMessage()))
 
 
 class LIMSClient(BaseService):
@@ -111,7 +98,7 @@ class LIMSClient(BaseService):
         self.address = address
         self.cookies = {}
         self.set_state(active=True)
-        _logger.info('MxLIVE Service configured for %s' % (address))
+        _logger.info('MxLIVE Service configured for {}'.format(address))
 
     def is_ready(self):
         return True
@@ -119,34 +106,33 @@ class LIMSClient(BaseService):
     def get(self, *args, **kwargs):
         r = requests.get(*args, verify=False, cookies=self.cookies, **kwargs)
         if r.status_code == requests.codes.ok:
-            reply = r.json()
+            return r.json()
         else:
             r.raise_for_status()
-        return reply
 
     def post(self, *args, **kwargs):
         r = requests.post(*args, verify=False, cookies=self.cookies, **kwargs)
         if r.status_code == requests.codes.ok:
-            reply = r.json()
+            return r.json()
         else:
-            print r.text
+            _logger.error('Failed posting data to MxLIVE: HTTP-{}'.format(r.status_code))
             r.raise_for_status()
-        return reply
 
     def get_project_samples(self, beamline):
+        _logger.debug('Requesting Samples from MxLIVE ...')
         url = "{}/api/{}/samples/{}/{}/".format(
-            self.address, beamline.config.get('lims_api_key', ''), beamline.name, misc.get_project_name()
+            self.address, beamline.config.get('lims_api_key', ''), beamline.name, get_project_name()
         )
         try:
             reply = self.get(url)
         except (IOError, ValueError, requests.HTTPError) as e:
-            _logger.error('Unable to fetch Samples from MxLIVE: \n %s' % e)
+            _logger.error('Unable to fetch Samples from MxLIVE: \n {}'.format(e))
             reply = {'error': 'Unable to fetch Samples from MxLIVE', 'details': '{}'.format(e)}
         return reply
 
     def upload_dataset(self, beamline, data):
         url = "{}/api/{}/data/{}/{}/".format(
-            self.address, beamline.config.get('lims_api_key', ''), beamline.name, misc.get_project_name()
+            self.address, beamline.config.get('lims_api_key', ''), beamline.name, get_project_name()
         )
 
         json_info = {
@@ -169,7 +155,7 @@ class LIMSClient(BaseService):
             'staff_comments': data.get('comments'),
         }
         if data.get('crystal_id'):
-            json['crystal_id'] = int(data['crystal_id'])
+            json_info['crystal_id'] = int(data['crystal_id'])
         if data['num_frames'] < 10:
             json_info['kind'] = 0  # screening
         else:
@@ -178,7 +164,7 @@ class LIMSClient(BaseService):
         if data['num_frames'] >= 2:
             try:
                 reply = self.post(url, json=json_info)
-            except (IOError, ValueError, requests.HTTPError), e:
+            except (IOError, ValueError, requests.HTTPError) as e:
                 _logger.error('Dataset meta-data could not be uploaded to MxLIVE, \n {}'.format(e))
             else:
                 if reply.get('id') is not None:
@@ -187,7 +173,7 @@ class LIMSClient(BaseService):
                 else:
                     _logger.error('Invalid Response from MxLIVE: {}'.format(reply))
 
-        with open(os.path.join(data['directory'], '%s.SUMMARY' % data['name']), 'w') as fobj:
+        with open(os.path.join(data['directory'], '{}.SUMMARY'.format(data['name'])), 'w') as fobj:
             json.dump(data, fobj, indent=4)
 
         return
@@ -202,6 +188,7 @@ class LIMSClient(BaseService):
             self.address, beamline.config.get('lims_api_key', ''), beamline.name, get_project_name()
         )
         kind = int(scan.get('kind'))
+        new_info = {}
 
         if kind == 1:  # Excitation Scan
             crystal_id = None if not scan['parameters'].get('crystal_id', '') else int(scan['parameters']['crystal_id'])
@@ -240,7 +227,7 @@ class LIMSClient(BaseService):
 
         try:
             reply = self.post(url, json=new_info)
-        except (IOError, ValueError, requests.HTTPError), e:
+        except (IOError, ValueError, requests.HTTPError) as e:
             _logger.error('Scan could not be uploaded to MxLIVE, \n {}'.format(e))
         else:
             if reply.get('id'):
@@ -258,7 +245,7 @@ class LIMSClient(BaseService):
         if report.get('data_id'):
             try:
                 reply = self.post(url, json=report)
-            except (IOError, ValueError, requests.HTTPError), e:
+            except (IOError, ValueError, requests.HTTPError) as e:
                 msg = 'Report could not be uploaded to MxLIVE, \n {}'.format(e)
                 _logger.error(msg)
                 reply = {'error': msg}
@@ -279,10 +266,83 @@ class LIMSClient(BaseService):
             report = data['result']
             self.upload_report(beamline, report)
 
-        if len(reports):
-            with open(os.path.join(report['url'], 'process.json'), 'w') as fobj:
+        if reports:
+            result = reports[0]['result']
+            with open(os.path.join(result['url'], 'process.json'), 'w') as fobj:
                 info = {'result': reports, 'error': None}
                 json.dump(info, fobj)
 
         return reports
 
+
+class MxDCClient(BaseService):
+    def __init__(self, service_type):
+        BaseService.__init__(self)
+        self.name = "Remote MxDC"
+        self._service_found = False
+        self.added_id = None
+        self.removed_id = None
+        self.service_data = {}
+        self.service = None
+        self._ready = False
+        self.service_type = service_type
+        gobject.idle_add(self.setup)
+
+    def on_service_added(self, obj, data):
+        self._service_found = True
+        self.service_data = data
+        self.factory = ServerClientFactory()  # pb.PBClientFactory()
+        self.factory.getRootObject().addCallback(self.on_connected).addErrback(self.on_connection_failed)
+        reactor.connectTCP(self.service_data['address'], self.service_data['port'], self.factory)
+
+        _logger.warning(
+            'Remote MXDC instance {}@{}:{} since {}'.format(
+                self.service_data['data']['user'], self.service_data['address'], self.service_data['port'],
+                self.service_data['data']['started']
+            )
+        )
+
+    def on_service_removed(self, obj, data):
+        # if not self._service_found and self.service_data['address'] == data['address']:
+        #     return
+        self._service_found = False
+        self._ready = False
+        self.set_state(active=False)
+        self.notify_failure()
+
+    def setup(self):
+        """Find out the connection details of the Remove MXDC using mdns
+        and initiate a connection"""
+        self.browser = mdns.Browser(self.service_type)
+        self.added_id = self.browser.connect('added', self.on_service_added)
+        self.removed_id = self.browser.connect('removed', self.on_service_removed)
+        gobject.timeout_add(2000, self.notify_failure)
+
+    def on_connected(self, perspective):
+        """ I am called when a connection to the MxDC instance has been established.
+        I expect to receive a remote perspective which will be used to call remote methods
+        on the MxDC instance."""
+        self.service = perspective
+        self.service.notifyOnDisconnect(self.on_disconnected)
+        self._ready = True
+        self.set_state(active=True)
+
+    def on_disconnected(self, remote):
+        """Used to detect disconnections if MDNS is not being used."""
+        self.set_state(active=False)
+
+    def on_connection_failed(self, reason):
+        reason.trap(error.ConnectionDone)
+        _logger.warning('Connection to Remote MxDC Failed')
+
+    def notify_failure(self):
+        if not self.active_state:
+            self.browser.disconnect(self.removed_id)
+            self.browser.disconnect(self.added_id)
+            self.set_state(health=(16, 'not-connected'))
+
+    def is_ready(self):
+        return self._ready
+
+
+__all__ = ['DPMClient', 'MxDCClient', 'LIMSClient']
