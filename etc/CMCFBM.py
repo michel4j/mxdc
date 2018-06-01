62f77c95ece99b09e90388e10ff51c5bd753bbca
Michel Fodje <michel.fodje@lightsource.ca>
2018-06-01 16:33:09 -0600

    Fixed capillary centering

--- etc/CMCFBM.py
+++ etc/CMCFBM.py
@@ -27,7 +27,7 @@ CONFIG = {
     'xrf_energy_offset': 2.0,
     'shutter_sequence': ('ssh1', 'psh1', 'psh2', 'ssh3'),
     'orientation': 'left',
-    'centering_backlight': 15,
+    'centering_backlight': 20,
     'bug_report': ['michel.fodje@lightsource.ca']
 }
 
