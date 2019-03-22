07046139de33b94547134c84564aa589b5fcfdaf
Michel Fodje <michel.fodje@lightsource.ca>
2019-03-22 10:17:18 -0600

    Increase size of MxDC Window slightly to improve video display

--- mxdc/widgets/imageviewer.py
+++ mxdc/widgets/imageviewer.py
@@ -50,7 +50,7 @@ class ImageViewer(Gtk.Alignment, gui.BuilderMixin):
         'detector_type': '{}',
     }
 
-    def __init__(self, size=512):
+    def __init__(self, size=640):
         super(ImageViewer, self).__init__()
         self.setup_gui()
         self.set(0.5, 0.5, 1, 1)
