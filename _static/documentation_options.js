935db8cba3bf380b731158191388c2d5778dbcc0
Michel 4J <michel4j@gmail.com>
2020-05-11 13:19:39 -0600

    Rebuilt documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020.5.51',
+    VERSION: '2020.5.57',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
